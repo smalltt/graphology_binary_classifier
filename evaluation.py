706dc6e8c2ffa4741beed2030b70bd62246bcb74
Yufang Tang <tangyufang@outlook.com>
2023-03-20 15:45:21 -0400

    show label not 0 in inference.py; add f1-score in evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 import conf
 from common_utils import dataset as ds
+from sklearn.metrics import classification_report
 
 if __name__=="__main__":
     # load trained model
@@ -17,3 +18,21 @@ if __name__=="__main__":
     score = loaded_model.evaluate(test_ds, verbose=1)  
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+
+    # get label from test_ds
+    y_test = []
+    for y_test_image, y_test_label in test_ds.take(-1):
+        print('='*9)
+        print('y_test_image shape: ', y_test_image.numpy().shape)
+        print('y_test_label: ', y_test_label)  # tf.Tensor([1 1 0 0 1 0 0 1 1 1 1 0 1 1 0 1 0 0 0 0], shape=(20,), dtype=int32)
+        print('y_test_label.numpy: ', y_test_label.numpy())
+        y_test = y_test + y_test_label.numpy().tolist()
+        print('y_test', y_test)
+    
+    # evaluate the network
+    out = loaded_model.predict(test_ds)
+    print('!'*9)
+    print('out.argmax(axis=1): ', out.argmax(axis=1))  # [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+
+    print("[INFO] evaluating network...")
+    print(classification_report(y_test, out.argmax(axis=1)))
--- inference.py
+++ inference.py
@@ -13,7 +13,7 @@ if __name__=="__main__":
 
     # load image for inference
     img = keras.preprocessing.image.load_img(
-        "data/eg1/testing/conscientiousness/7.jpg", target_size=conf.input_im_size
+        "data/dataset_gra/no_split/testing/conscientiousness/7.jpg", target_size=conf.input_im_size
     )
     img_array = keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)  # Create batch axis
@@ -21,4 +21,4 @@ if __name__=="__main__":
     # run inference
     predictions = loaded_model.predict(img_array)
     score = float(predictions[0])
-    print(f"This image is {100 * (1 - score):.2f}% 0 and {100 * score:.2f}% 1.")
+    print(f"This image is {100 * (1 - score):.2f}% 0(conscientiousness) and {100 * score:.2f}% 1(extraversion).")
